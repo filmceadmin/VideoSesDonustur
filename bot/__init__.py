e7da8b88e882260b4cb9bac4eb56a2c3cb30616c
filmceadmin <90059885+filmceadmin@users.noreply.github.com>
2021-09-10 01:07:38 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -9,7 +9,10 @@ api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+sudo_users = for x in os.environ.get("ADMİNS", "").split())):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
@@ -19,3 +22,4 @@ if not download_dir.endswith("/"):
   download_dir = str(download_dir) + "/"
 if not os.path.isdir(download_dir):
   os.makedirs(download_dir)
+
